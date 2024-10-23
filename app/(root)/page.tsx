1cb00b54f19d332e08eabe2036af38cfed31456d
Nihal <nihal7u@gmail.com>
2024-10-23 19:30:37 +0530

    Impliment initial file and folder sturcture

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RootLayout from '../layout'
+
+const Home = () => {
+  return (
+    <RootLayout>
+      <p>SIDEBAR</p>
+      <div>Home</div>
+    </RootLayout>
+  )
+}
+
+export default Home;
